4ab866c3fd7a877b6e6a1601715f0c3137eb1ee2
Tringapps-Aarif <moin.mohammed@tringapps.com>
2023-03-06 10:15:37 +0530

    ok

--- src/main/java/details/DetailsBank.java
+++ src/main/java/details/DetailsBank.java
@@ -3,17 +3,17 @@ package details;
 import java.util.*;
 
   public class DetailsBank {
-     int accno;
+     int account;
    int bal;
    protected int currentBalance = 0;
   protected int depAmt;
 protected int wAmt;
-    String name;
+      String customerName;
     Scanner sc = new Scanner(System.in);
 
     public DetailsBank(int accno, String name) {
-        this.accno = accno;
-        this.name = name;
+        account = accno;
+        customerName = name;
     }
 
 }
